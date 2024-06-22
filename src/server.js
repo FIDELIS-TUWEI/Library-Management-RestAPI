ccd398629de65c6b3543623a5442b2d67b292430
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-22 12:04:01 +0300

    database connection logic import

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const app = require("./app");
+const connectMongoDB = require("./db/MongoDB");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
-
 app.listen(config.PORT, () => {
+    connectMongoDB();
     logger.info(`Server running on port: ${config.PORT}`)
 });
