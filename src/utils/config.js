6cb781a21e5dc59674d74e0147aac76c80f42b62
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-22 12:55:12 +0300

    additional config

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
+let NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
-    PORT, MONGODB_URI
+    PORT, MONGODB_URI, NODE_ENV
 };
