ac20b4f4a760c5def580afcfaa8efa8ed974be08
FIDELIS-TUWEi <fideliofidel9@gmail.com>
2024-06-22 11:57:37 +0300

    app setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const indexRoutes = require("./routes/index");
+
+const app = express();
+
+app.use(express.json({ limit: "5mb" })); // parse req.body
+app.use(express.urlencoded({ extended: true })); // parse form data(urlencoded)
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.disable("x-powered-by"); // disable express server fingerprinting
+
+app.use("/api/v1", indexRoutes);
+
+
+module.exports = app;
