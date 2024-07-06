22b4d5946de6f7d5cf25da6d50792b11e60dd1c5
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-07-06 07:40:43 +0300

    borrow model schema

---
+++ src/models/borrow.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const borrowSchema = new mongoose.Schema({
+    user: {
+        type: ObjectId,
+        ref: "User",
+        required: true
+    },
+    book: {
+        type: ObjectId,
+        ref: "Book",
+        required: true
+    },
+    borrowDate: {
+        type: Date,
+        default: Date.now(),
+    },
+    returnDate: {
+        type: Date
+    }
+},
+{ timestamps: { createdAt: "Date_created", updatedAt: "Date_updated" } },
+);
+
+module.exports = mongoose.model("Borrow", borrowSchema)
