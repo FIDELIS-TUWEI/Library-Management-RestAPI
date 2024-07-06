c9f3f2a5c4ea540c643bc1553035e354fd1a3ab0
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-07-06 07:41:09 +0300

    checkrole middleware

---
+++ src/middleware/checkRole.js
@@ -0,0 +1,14 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user.model");
+
+const checkRole = (roles) => asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+
+    if (!roles.includes(user.role)) {
+        return res.status(403).json({ status: "error", message: "Forbidden" });
+    }
+
+    next();
+});
+
+module.exports = { checkRole };
