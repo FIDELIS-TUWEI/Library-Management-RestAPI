c59b14c86a72c430dfbf48d6b93c5161d07cdb5a
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-07-06 05:38:54 +0300

    login  controller logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const logger = require("../utils/logger");
 const User = require("../models/user.model");
+const generateTokenAsCookie = require("../utils/generateToken");
 
 // Register user
 // @desc
@@ -50,7 +52,41 @@ const signup = asyncHandler (async (req, res) => {
 // Login user
 // @desc
 // @Access /auth/login
-const login = asyncHandler (async (req, res) => {});
+const login = asyncHandler (async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ status: "error", message: "Invalid email or password" });
+        };
+
+        // check if password matches in DB
+        const ispasswordMatch = await bcrypt.compare(password, user?.password);
+        if (!ispasswordMatch) {
+            return res.status(400).json({ status: "error", message: "Invalid email or password" });
+        };
+
+        // generate token as cookie
+        generateTokenAsCookie(user._id, res);
+
+        res.status(200).json({
+            status: "success",
+            message: "Logged in successfully",
+            data: {
+                _id: user._id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+            }
+        });
+
+    } catch (error) {
+        logger.error("Error in login controller", error);
+        return res.status(500).json({ status: "error", message: error.message || "Internal Server Error" });
+    }
+});
 
 module.exports = {
     signup, login
