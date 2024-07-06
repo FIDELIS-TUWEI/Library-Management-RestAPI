23442e21478f86af7e0229670f9a12c1aee82ee7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-07-06 06:29:41 +0300

    authentication logic complete

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,11 +3,11 @@ const config = require("./config");
 
 const generateTokenAsCookie = (userId, res) => {
     const token = jwt.sign({ userId }, config.JWT_SECRET, {
-        expiresIn: '1d'
+        expiresIn: '2d'
     });
 
     res.cookie("jwt", token, {
-        maxAge: 24 * 24 * 60 * 60 * 1000, // MS
+        maxAge: 2 * 24 * 60 * 60 * 1000, // MS
         httpOnly: true, // prevent XSS attackscross-site scripting attacks
         sameSite: "strict", // CSRF attacks cross-site request forgery attacks
         secure: config.NODE_ENV !== 'development' 
