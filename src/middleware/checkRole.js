57c2a0e12957c8eb4c73487251c8e0f54161183b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-07-06 07:56:24 +0300

    checkrole middleware

--- src/middleware/checkRole.js
+++ src/middleware/checkRole.js
@@ -5,7 +5,7 @@ const checkRole = (roles) => asyncHandler(async (req, res, next) => {
     const user = await User.findById(req.user._id);
 
     if (!roles.includes(user.role)) {
-        return res.status(403).json({ status: "error", message: "Forbidden" });
+        return res.status(403).json({ status: "error", message: "Forbidden: You're not authorized to perform this action!" });
     }
 
     next();
