b71927191a8bf287711fe4a44205cbced5d869ff
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-07-06 06:27:38 +0300

    added cookieparseer middleware

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const indexRoutes = require("./routes/index");
 
 const app = express();
 
 app.use(express.json({ limit: "5mb" })); // parse req.body
 app.use(express.urlencoded({ extended: true })); // parse form data(urlencoded)
+app.use(cookieParser());
 
 app.use(morgan("dev"));
 app.use(cors());
